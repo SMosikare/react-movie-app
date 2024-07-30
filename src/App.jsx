f16c73aba749f5c51eb0216313b1a907ef6068c2
SMosikare <salomemosikare25@gmail.com>
2024-07-30 19:26:48 +0200

    Add header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-
+import searchIcon from "./assets/search-icon.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>MovieVille</h1>
+
+        <div className="search-bar">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search movies"
+          />
+
+          <button className="search-button">
+            <img src={searchIcon} alt="Search movies" className="search-icon" />
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
